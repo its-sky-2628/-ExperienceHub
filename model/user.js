3df4207c463ec19e4ad1bcbf80cfc676245c3a00
SHREYANSH YADAV <shreyanshyadav@SHREYANSHs-MacBook-Air.local>
2026-06-25 14:40:19 +0530

    Added Render env variable support

--- model/user.js
+++ model/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/miniproject");
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
 
 const UserSchema = mongoose.Schema({
     username: String,
