9e97979308994db0e3d8021aa84511fd25b0fb2c
SHREYANSH YADAV <shreyanshyadav@SHREYANSHs-MacBook-Air.local>
2026-07-06 22:05:14 +0530

    Updated login page

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB Atlas Connected"))
 .catch(err => console.log(err));
-
-const app = express();
 const app = express();
 const bcrypt=require('bcrypt');
 const jwt=require("jsonwebtoken");
