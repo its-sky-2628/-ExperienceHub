ad62c7461e1ff6e7ff596c7b5650eab28d2d9382
SHREYANSH YADAV <shreyanshyadav@SHREYANSHs-MacBook-Air.local>
2026-07-15 17:45:38 +0530

    Updated login page

--- config/mailer.js
+++ config/mailer.js
@@ -1,18 +1,19 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-
-    host: "smtp.gmail.com",
-
-    port: 587,
-
-    secure: false,
-
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-
+  // smtp.gmail.com की जगह डायरेक्ट Google का IPv4 SMTP सर्वर एड्रेस
+  host: "74.125.195.108", 
+  port: 587,
+  secure: false, // TLS के लिए false ही रखें
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // आपका 16-digit App Password
+  },
+  tls: {
+    rejectUnauthorized: false,
+    servername: "smtp.gmail.com" // SSL/TLS हैंडशेक के लिए यह डालना ज़रूरी है
+  }
 });
 
 module.exports = transporter;
